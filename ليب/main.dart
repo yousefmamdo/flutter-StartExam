e9fd0eb82baf0b0aa1409431b7ddee8d16a5eecf
yousef mando <85874237+yousefmamdo@users.noreply.github.com>
2022-06-01 16:44:16 +0300

    Update and rename lib/main.dart to ليب/main.dart

--- ليب/main.dart
+++ ليب/main.dart
@@ -13,12 +13,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Home',
       theme: ThemeData(
      
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Home Page'),
     );
   }
 }
