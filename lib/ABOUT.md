9e1374c77c5f77b627ddc967a64a5fe6dd2d7bd4
yousef mando <85874237+yousefmamdo@users.noreply.github.com>
2022-06-01 04:53:21 +0300

    added

---
+++ lib/ABOUT.md
@@ -0,0 +1,12 @@
+## What is Flutter?
+
+***Back in the days of Objective C/Swift and Java/Kotlin as primary languages for mobile development, building apps was expensive. You had to build two separate apps, which obviously meant doing the work twice.To solve this problem, several frameworks have been constructed for the creation of hybrid (or cross-platform) apps in HTML5 and Javascript. Among the cross-platform toolkits, including Phonegap, Xamarin, React Native, and more, the Flutter framework has quickly become increasingly popular among developers, enterprises, entrepreneurs and users.Flutter is a portable UI toolkit for building native-like apps across mobile, web and desktop, from a single codebase. It uses the programming language Dart and incorporates Material Design and Cupertino widgets. Flutter developers can create spectacular UI that looks and feels native. It behaves naturally on any platform, even though you’re using one codebase.Flutter is the only framework with a mobile SDK that provides a responsive style without using a Javascript bridge, thereby reaching a level of performance that rivals its cousin and direct competitor React Native. It easily integrates with the different platforms such as Android, IOS and Linux, MAC, Windows and Google Fuchsia applications.***
+  
+## Why choose a Flutter?
++ Write Code Faster
++ Write One Code for All Platforms, Save Time and Money  
++ Flutter Apps Work Faster  
++ Worry Less about Testing  
++ Customizable Widgets for Beautiful UI
++ Your App Looks the Same on Older Devices Too
++ Ample Libraries and Excellent Documentation
---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+     
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+ 
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+Future<List<Countries>> _getCountries() async {
+  var data =await http.get(Uri.parse('https://countriesnow.space/api/v0.1/countries/info?returns=currency,flag,unicodeFlag,dialCode'));
+  var jsonData =json.decode(data.body);
+  List<Countries> country=[];
+  for(var i in jsonData){
+    Countries countries=Countries(i['name'],i['currency'],i['unicodeFlag'],i['flag'],i['dialcode']);
+    country.add(countries);
+  }
+  return country;
+}
+ 
+  @override
+  Widget build(BuildContext context) {
+  
+    
+    return Scaffold(
+      appBar: AppBar(
+     
+        title: Text(widget.title),
+      ),
+      body: Container(
+        child: FutureBuilder(
+          future: _getCountries(),
+        builder: (context,snapshot){
+          if(snapshot.data==null){
+            return Container(
+              child: Center(child: Text('Loading...')),
+            );
+          }else{
+          return ListView.builder(itemCount: snapshot.data.length,
+          itemBuilder: (context, index){
+            return ListTile(
+              leading: CircleAvatar(backgroundImage: NetworkImage(snapshot.data[index].flag)),
+              title: Text(snapshot.data![index].name),
+              subtitle: Text(snapshot.data[index].currency),
+            
+            );
+          },
+
+          
+          );
+          }
+        },
+        
+        ),
+
+      ),
+    );
+  }
+}
+
+
+class Countries {
+  late final String name;
+   late final String currency;
+   late final String unicodeFlag;
+   late final String flag;
+   late final String dialcode;
+
+Countries(this.name,this.currency,this.unicodeFlag,this.flag,this.dialcode);
+
+}
